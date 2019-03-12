b424119c9c63b4eafd4a2b057be8fd18a474d693
Jen <jennifer.m.essery@gmail.com>
2019-03-12 20:33:08 +0000

    Add horoscope functionality

--- flask_app/main_flask.py
+++ flask_app/main_flask.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from horoscopeapi import get_horoscope, zodiac_sign
 
 app = Flask("MyApp")
 
@@ -36,10 +37,21 @@ def hello_someone(name):
 @app.route("/yourgeneration",methods=["POST"])
 def showgeneration():
     form_data = request.form
-    year = form_data["dob"]
+    year = form_data["year"]
     try:
-        return render_template("gen.html", gen=calcgen(int(year)).title())
+        return render_template("gen.html", gen=calcgen(int(year)).title(), year=year)
     except:
         return "Please input a valid year."
+
+@app.route("/horoscope",methods=["POST"])
+def showhoroscope():
+    form_data = request.form
+    day = form_data["day"]
+    month = form_data["month"]
+    data = get_horoscope(zodiac_sign(int(month),int(day)))
+    try:
+        return render_template("horoscope.html", data=data, day=day, month=month)
+    except:
+        return "Please input a valid day & month."
 
 app.run(debug=True)
